d213631393d549500604905483d413b55203c656
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-16 18:55:36 +0500

    aginas

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express();
+const PORT = 4545
 
 app.get('/', function(req, res){
     console.log(`This server started on Port ${PORT}`);
     res.send('Hello World!')
 });
+app.post('/', function(req, res){
+    console.log(req);
+    res.send('POST')
+});
 
-app.listen(4545)
+app.listen(PORT)
