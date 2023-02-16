230ea577bf1a373b0e019617204e18e9b7cf6e72
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-16 19:18:49 +0500

    changes about data

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express();
 const PORT = 4545
+const user = require('./user')
+app.use(express.json())
 
-app.get('/', function(req, res){
-    console.log(`This server started on Port ${PORT}`);
-    res.send('Hello World!')
-});
+// app.get('/', function(req, res){
+//     console.log(`This server started on Port ${PORT}`);
+//     res.send('Hello World!')
+// });
+// app.post('/', function(req, res){
+//     console.log(req.body);
+//     res.send('POST')
+// });
 app.post('/', function(req, res){
-    console.log(req.body);
-    res.send('POST')
+    const newUser = req.body
+    user.push(newUser)
+    res.status(201).json({
+        status: "success",
+        message: "Added New User",
+        error: null,
+        data:[
+            user
+        ]
+    })
 });
 
 app.listen(PORT)
---
+++ user.js
@@ -0,0 +1,54 @@
+const users = [
+    {
+        name: "Ali",
+        surname: "Azimov",
+        age: "23",
+    },
+    {
+        name: "Alim",
+        surname: "Av",
+        age: "23",
+    },
+    {
+        name: "Aziz",
+        surname: "Azim",
+        age: "23",
+    },
+    {
+        name: "Amir",
+        surname: "Azimo",
+        age: "23",
+    },
+    {
+        name: "Ahmad",
+        surname: "Azv",
+        age: "23",
+    },
+    {
+        name: "Alisher",
+        surname: "Aziv",
+        age: "23",
+    },
+    {
+        name: "Akmal",
+        surname: "Azim",
+        age: "23",
+    },
+    {
+        name: "Akbar",
+        surname: "Alam",
+        age: "23",
+    },
+    {
+        name: "Abdulloh",
+        surname: "Aliyev",
+        age: "23",
+    },
+    {
+        name: "Anim",
+        surname: "Azizov",
+        age: "23",
+    }
+]
+
+module.exports=users
