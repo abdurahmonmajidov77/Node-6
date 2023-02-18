7ba1f4cb97ca29a6447a8f3d1c833895da40c5a9
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-18 17:03:26 +0500

    changed

--- Home/index.js
+++ Home/index.js
@@ -3,6 +3,14 @@ const fs = require('fs')
 const app = express()
 const PORT = 8080
 
+function reader() {
+    const data = fs.writeFileSync('./db.json', 'utf-8',(err, data) => {
+        if(err) throw err
+        return d
+    })
+    return JSON.parse(data)
+}
+
 function writer(data) {
     const d = JSON.stringify(data)
     fs.writeFileSync('./db.json', d,(err) => {
@@ -11,30 +19,22 @@ function writer(data) {
     })
 }
 
-const Data = fs.readFileSync('./Home/db.js', 'utf-8', (err, data) => {
-    if(err) throw err
-    return data
-})
+// const Data = fs.readFileSync('./Home/db.js', 'utf-8', (err, data) => {
+//     if(err) throw err
+//     return data
+// })
 
 app.get('/', function(req, res){
     console.log('GET');
-    res.send(Data)
+    const data = reader()
+    res.status(201).json({
+        data
+    })
 })
 app.post('/', function(req, res){
     console.log('POST');
-    const postData = Data.push(req.body)
-    fs.writeFileSync('./db.js', `${postData}`, (err, data) => {
-        if(err) throw err
-        return data
-    })
-    res.status(201).json({
-        status: "success",
-        message: "Posted User",
-        error: null,
-        data:[
-            user
-        ]
-    })
+    const data = reader()
+    data.push()
 })
 // app.delete('/', function(req, res){
 //     console.log('DELET');
